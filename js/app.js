34b47b20ef8f18bacf7e10bc707bb463dc1a9ec0
Mark Davis <markdavisbrooklyn@gmail.com>
2015-05-26 19:37:07 -0400

    fixed all but one of the bugs

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 
     //At the beginning of each game, create two things
     //A random number between 1-100, which will be the answer for this game 
-    var answer = $(Math.floor((Math.random() * 100) + 1));
+    var answer = Math.floor(Math.random() * 100) + 1;
+
     //An empty array to store the user's guesses for this game
     var myGuessesArray = [];
     //A variable to track the number of guesses for this game
@@ -27,20 +28,21 @@ $(document).ready(function() {
         if (myNewGuess < 1 || myNewGuess > 100) {
             alert("Please enter a real number between 1 and 100");
             return false;
-        }
+        };
 
         //2. Validate that the input is not a repeat
-        // if (myGuessesArray.indexOf(myNewGuess) === -1) {
-        // 	alert("You already guessed that number!");
-        // 	return false;
-        //};
+        if (myGuessesArray.indexOf(myNewGuess) !== -1) {
+            alert("You already guessed that number!");
+            return false;
+        };
 
+//BUG #1: this is not working...when I got the right answer 38, it said "Your guess of 38 was hot. You should guess lower."
         //3. Validate to see if the user guessed the right answer
         if (myNewGuess === answer) {
             //Change the background color, add an image, or do something creative 	
             $("youwin").addClass("youWinSuccessMessageOn");
             return false;
-        }
+        };
 
         //If we make it through those validation checks, continue with the rest of the program 
 
@@ -57,17 +59,15 @@ $(document).ready(function() {
         //calculate the absolute value of howFarOff and store it in a variable
         var howFarOverAbsolute = Math.abs(howFarOver);
       	var hotWarmCold = "";
-//BUG #1: IT ALWAYS SAYS I'M COLD, NEVER HOT OR WARM:
+
         if (howFarOverAbsolute < 10) {
             hotWarmCold = "hot";
-        } else if (howFarOverAbsolute < 90) {
+        } else if (howFarOverAbsolute < 30) {
             hotWarmCold = "warm";
         } else {
             hotWarmCold = "cold";
         }
 
-
-//BUG #2: IT ALWAYS SAYS I NEED TO GUESS HIGHER:
         var higherOrLower = "";
         if (myNewGuess < answer) {
             higherOrLower = "higher";
@@ -89,30 +89,32 @@ $(document).ready(function() {
             $("#guessLog4").append(guessLogMessage);                  
         } else {
             //if the user has now made his 5 guesses, then the game is over
-
-//BUG #3: ON THE SCREEN IT'S DISPLAYING: 
-//"Your guess of 100 was cold. You have now used up your 5 guesses, so this game is now over. The answer was [object Object]."
-//It should display the answer instead of "[object bject]"
             var guessLogMessageLast = "Your guess of " + myNewGuess + " was " + hotWarmCold + ".  You have now used up your 5 guesses, so this game is now over. The answer was " + answer + "."; //
-            $("#guessLog5").append(guessLogMessageLast); 
-//BUG #4: NEED TO MAKE THE GAME STOP AT THIS POINT:                
+            $("#guessLog5").append(guessLogMessageLast);             
             return false;
         }
 
         //Create a button that provides the answer (Give me a Hint).
         $("#giveHint").click(function() {
-//BUG #5: NEED TO FIX THE "object Object" ERROR HERE TOO:
             alert("The answer was " + answer + ". This game is now over...but feel free to play again by clicking the 'New Game' button.");
             return false;
         });
-
     });
 
-//BUG #6: THIS IS NOT WORKING, IS THERE A BETTER WAY?
         //Create a "new game" button that resets the game:
         $("#newGame").click(function() {
-            answer = $(Math.floor((Math.random() * 100) + 1));
+            answer = Math.floor((Math.random() * 100) + 1);
             myGuessesArray = [];
+            numberOfGuesses = 0;
+            //remove the last guess that I entered:
+            $("#myNewGuess").val('');
+            //remove any guess logs:
+            $("#guessLog1").empty();
+            $("#guessLog2").empty();
+            $("#guessLog3").empty();
+            $("#guessLog4").empty();
+            $("#guessLog5").empty();
+            //remove the You Win message:
             $("youwin").removeClass("youWinSuccessMessageOn");
             $("youwin").addClass("youWinSuccessMessageOff");
             alert("The game has started over.  Enter your first guess!");
